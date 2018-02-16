2db49ca91952ebcaa9fcca28c489ce043ab6bc77
Brann <dizeasd@gmail.com>
2018-02-16 14:58:10 +0100

    Les_02
    kleur toegevoegd

---
+++ unity/New Unity Project/Assets/les_02/color.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class color : MonoBehaviour {
+
+	public Color cubecolor = new Color();
+	public Renderer cuberenderer;
+	public GameObject player;
+	public float lerpstrength;
+
+	// Use this for initialization
+	void Start () {
+		//cuberenderer.material.color = cubecolor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//verandert kleur gebaseerd op afstand. getyped door iemand anders
+		cubecolor = new Color (cubecolor.r, Vector3.Distance (transform.position, player.transform.position) / lerpstrength, Vector3.Distance (transform.position, player.transform.position) / lerpstrength);
+		cuberenderer.material.color = cubecolor;
+	}
+}
