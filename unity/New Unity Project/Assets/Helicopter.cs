829ce8094c5aa967f9e7cd96dcb50d3d5579ed41
Brann <dizeasd@gmail.com>
2018-02-16 13:37:14 +0100

    Unity Project
    Project aangemaakt

---
+++ unity/New Unity Project/Assets/Helicopter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Helicopter : MonoBehaviour
+{
+	
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = new Vector3 (transform.position.x, transform.position.z, transform.position.y + 0.2f);
+		transform.eulerAngles = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z + 5f);
+
+	}
+}
+
